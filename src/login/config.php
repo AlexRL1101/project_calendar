0cfb52fe7b432fdb42bccc6509de2d3dce78435f
AlexRL1101 <la.rebel.del.pebetero@gmail.com>
2025-08-21 01:25:35 -0600

    Cambios

--- src/login/config.php
+++ src/login/config.php
@@ -3,14 +3,22 @@ $host = getenv('DB_HOST');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
 $db   = getenv('DB_NAME');
-$port   = getenv('DB_PORT');
+$port = getenv('DB_PORT');
 
 try {
-    $conn = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
+
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        // Fuerza conexión SSL
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+        PDO::MYSQL_ATTR_SSL_CA => __DIR__ . "../assets/cert/cacert.pem" // ruta al certificado CA
+    ];
+
+    $conn = new PDO($dsn, $user, $pass, $options);
+
+    echo "✅ Conexión exitosa con SSL";
 } catch(PDOException $e) {
     print_r($e);
-    die('No se puede conectar a la base de datos');
+    die('❌ No se puede conectar a la base de datos');
 }
-
-?>
